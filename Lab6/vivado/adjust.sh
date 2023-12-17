69d8d58b4165f9c3f249555c4dd77b92d2e01e7d
Charlee <85032763+Charlee0207@users.noreply.github.com>
2023-12-17 23:03:35 +0800

    Delete Lab6 directory

--- Lab6/vivado/adjust.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mv ./jupyter_notebook/caravel_fpga.bit ./jupyter_notebook/design_1.bit
-mv ./jupyter_notebook/caravel_fpga.hwh ./jupyter_notebook/design_1.hwh
-
-cp ../testbench/lab6/lab6.hex -t ./jupyter_notebook
